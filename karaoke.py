ea30a807192369dc724e75b1475db45dd13ae1fc
arelanno <a.relano@alumnos.urjc.es>
2019-10-16 21:43:25 +0200

    creado init en karaoke.py

---
+++ karaoke.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from xml.sax import make_parser
+from smallsmilhandler import SmallSMILHandler
+import sys
+import urllib.request
+import json
+
+
+class KaraokeLocal():
