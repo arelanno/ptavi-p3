3ff2149b53aaa95c9cbc64dc5c59293c2aaa1e88
arelanno <a.relano@alumnos.urjc.es>
2019-10-16 03:36:24 +0200

    añadido clase de handler para sshandler.py

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
 
+
+class SmallSMILHandler(ContentHandler):
+
+    def __init__(self):
+        self.tag_list = []
+        self.rootlayout = ["width", "height", "background-color"]
+        self.region = ["id", "top", "bottom", "left", "right"]
+        self.img = ["src", "region", "begin", "dur"]
+        self.audio = ["src", "begin", "dur"]
+        self.textstream = ["src", "region"]
